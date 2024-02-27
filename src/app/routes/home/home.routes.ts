af5af276b58464574957bc74eecb97688439155a
Jerrol <jerrolkrause@gmail.com>
2024-02-27 13:28:44 -0500

    Copy updates, favicons

--- src/app/routes/home/home.routes.ts
+++ src/app/routes/home/home.routes.ts
@@ -1,14 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { HomeModule } from './home.module';
-import { titleAppendSlug } from '$shared';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    title: titleAppendSlug('Home'),
+    title: 'Life Insurance, Financial Planning & Annuities for First Responders',
   },
 ];
 
